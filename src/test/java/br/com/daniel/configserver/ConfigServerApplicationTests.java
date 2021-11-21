984fdc177d94ef2e75291cdf34afba321fbeba44
daniel <danielsena04@gmail.com>
2021-11-21 17:30:29 -0300

    first commit

---
+++ src/test/java/br/com/daniel/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
